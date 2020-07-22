45b9777945a61e68f03560fa70cdf82d11686da5
Pieter Houwen <pieter@pieterhouwen.info>
2020-07-22 15:18:41 +0200

    Update searchisp.sh

--- walloffail/modules/searchisp.sh
+++ walloffail/modules/searchisp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # DO NOT USE THIS SCRIPT, THIS WILL GET YOU BANNED FROM WHOIS
-echo Removing old masterisptable...
-rm /opt/zooi/networkoutput/masterisptable
-echo Beginning whois loop.
-for i in $(cat /opt/zooi/networkoutput/masteripsorted)
-do
-echo $i: $(whois $i | grep -i netname | cut -d ":" -f2) >>/opt/zooi/networkoutput/masterisptable
-done
-echo Ended whois loop!
+
+
+
+#echo Removing old masterisptable...
+#rm /opt/zooi/networkoutput/masterisptable
+#echo Beginning whois loop.
+#for i in $(cat /opt/zooi/networkoutput/masteripsorted)
+#do
+#echo $i: $(whois $i | grep -i netname | cut -d ":" -f2) >>/opt/zooi/networkoutput/masterisptable
+#done
+#echo Ended whois loop!
